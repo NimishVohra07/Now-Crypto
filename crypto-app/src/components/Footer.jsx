8e0497d766eb74c604906fe9d623a0b88254c57f
Nimish Vohra <136614864+NimishVohra07@users.noreply.github.com>
2023-07-10 13:54:01 +0530

    Add files via upload

---
+++ crypto-app/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import {  Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"black"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+          
+Crypto Guidance App is a leading mobile application that provides essential information and guidance on cryptocurrency investments. Our team of experts is dedicated to empowering individuals to make informed decisions and navigate the complex world of cryptocurrencies. 
+          </Text>
+        </VStack>
+
+        <VStack>
+        
+          
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
