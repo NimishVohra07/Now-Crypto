9b8f14b8f6a7193382fb15030bd74376d10d0cf5
Nimish Vohra <136614864+NimishVohra07@users.noreply.github.com>
2023-07-10 13:58:57 +0530

    Footer.jsx

--- crypto-app/src/components/Footer.jsx
+++ crypto-app/src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             textAlign={["center", "left"]}
           >
           
-Crypto Guidance App is a leading mobile application that provides essential information and guidance on cryptocurrency investments. Our team of experts is dedicated to empowering individuals to make informed decisions and navigate the complex world of cryptocurrencies. 
+Now Crypto App is a mobile application project that provides essential information and guidance on cryptocurrency investments. This will help empowering individuals to make informed decisions and navigate the complex world of cryptocurrencies. 
           </Text>
         </VStack>
 
